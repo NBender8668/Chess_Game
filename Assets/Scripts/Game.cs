739df0e4b468a1aeadb2833793e2c003d0935a71
NBender8668 <NBender@student.neumont.edu>
2022-02-14 14:09:26 -0700

    Chess960

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -26,7 +26,7 @@ public class Game : MonoBehaviour
     public bool isChess960;
 
     //random 
-    public List<int> whitepositionList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+    public List<int> whitepositions = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
 
     public List<int> blackpositons = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
 
@@ -37,15 +37,15 @@ public class Game : MonoBehaviour
         if(isChess960)
         {
             
-            playerWhite = new GameObject[] { Create("white_rook", PickPosition(whitepositionList), 0), Create("white_knight", PickPosition(whitepositionList), 0),
-            Create("white_bishop", PickPosition(whitepositionList), 0), Create("white_queen", PickPosition(whitepositionList), 0), Create("white_king", PickPosition(whitepositionList), 0),
-            Create("white_bishop", PickPosition(whitepositionList), 0), Create("white_knight", PickPosition(whitepositionList), 0), Create("white_rook", PickPosition(whitepositionList), 0),
+            playerWhite = new GameObject[] { Create("white_rook", PickPosition(whitepositions), 0), Create("white_king", PickPosition(whitepositions), 0),
+            Create("white_rook", PickPosition(whitepositions), 0), Create("white_bishop", PickPosition(whitepositions), 0), Create("white_knight", PickPosition(whitepositions), 0),
+            Create("white_knight", PickPosition(whitepositions), 0), Create("white_bishop", PickPosition(whitepositions), 0), Create("white_queen", PickPosition(whitepositions), 0),
             Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
             Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
             Create("white_pawn", 6, 1), Create("white_pawn", 7, 1) };
-            playerBlack = new GameObject[] { Create("black_rook", PickPosition(blackpositons), 7), Create("black_knight",PickPosition(blackpositons),7),
-            Create("black_bishop",PickPosition(blackpositons),7), Create("black_queen",PickPosition(blackpositons),7), Create("black_king",PickPosition(blackpositons),7),
-            Create("black_bishop",PickPosition(blackpositons),7), Create("black_knight",PickPosition(blackpositons),7), Create("black_rook",PickPosition(blackpositons),7),
+            playerBlack = new GameObject[] { Create("black_rook", PickPosition(blackpositons), 7), Create("black_king",PickPosition(blackpositons),7),
+            Create("black_rook",PickPosition(blackpositons),7), Create("black_bishop",PickPosition(blackpositons),7), Create("black_knight",PickPosition(blackpositons),7),
+            Create("black_knight",PickPosition(blackpositons),7), Create("black_bishop",PickPosition(blackpositons),7), Create("black_queen",PickPosition(blackpositons),7),
             Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6),
             Create("black_pawn", 3, 6), Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
             Create("black_pawn", 6, 6), Create("black_pawn", 7, 6) };
@@ -104,15 +104,26 @@ public class Game : MonoBehaviour
         
     }
 
-
     public int PickPosition(List<int> list)
     {
         var rand = new System.Random();
         int index = rand.Next(list.Count);
 
         var value = list[index];
-        
-        list.RemoveAt(index);
+
+        if(list[index] % 2 == 0)
+        {
+            rand.Next(list.Count);
+            list.RemoveAt(index);
+        }
+        else
+        {
+            rand.Next(list.Count);
+            list.RemoveAt(index);
+
+        }
+
+        Debug.Log(value);
 
         return value;
     }
